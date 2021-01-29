d44f2de5446b899cc4c01e4c55a02c6a21f62721
Igor Rom <yyy1407198888@gmail.com>
2021-01-29 22:35:17 +0400

    Кривинько, но зато сам))

--- src/main/java/DecryptionOfTheCaesar.java
+++ src/main/java/DecryptionOfTheCaesar.java
@@ -3,49 +3,47 @@ import java.util.List;
 
 public class DecryptionOfTheCaesar {
 
-    private final char[] al = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".toCharArray();
+    private final char[] alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".toCharArray();
     private String decryptionText = "";
-    private List<String> listDecryptionText = new ArrayList<String>();
+    private final List<String> listDecryptionText = new ArrayList<String>();
 
-    public void decrypt(String text){
+    public void decrypt(String text) {
 
-        for (int i = 1; i <= 33; i++) {
-            for (char c: text.toCharArray()) {
-                if (Character.isLetter(c)){
-                    char lower = Character.toLowerCase(c);
-                    for (int j = 0; j < al.length; j++) {
-                        if (lower == al[j]){
-                            int sum = j + i;
-                            decryptionText += checkLetter(sum,c);
+        for (int count = 1; count <= 33; count++) {
+            for (char c : text.toCharArray()) {
+                if (Character.isLetter(c)) {
+                    for (int i = 0; i < alphabet.length; i++) {
+                        if (Character.toLowerCase(c) == alphabet[i]) {
+                            int sum = i + count;
+                            decryptionText += checkLetter(sum, c);
                             break;
                         }
                     }
-                }else {
-                    decryptionText += (char)(c);
+                } else {
+                    decryptionText += (char) (c);
                 }
             }
             listDecryptionText.add(decryptionText);
             decryptionText = "";
         }
-        for (String ss: listDecryptionText) {
-            System.out.println(ss);
-            System.out.println();
+        for (String ss : listDecryptionText) {
+            System.out.println(ss + "\n");
         }
     }
 
-    private char checkLetter(int sum, char c){
+    private char checkLetter(int sum, char c) {
         char letter;
-        if (sum > 32){
-            if (Character.isUpperCase(c)){
-                letter = Character.toUpperCase(al[(sum) - 33]);
-            }else {
-                letter = al[(sum) - 33];
+        if (sum > 32) {
+            if (Character.isUpperCase(c)) {
+                letter = Character.toUpperCase(alphabet[(sum) - 33]);
+            } else {
+                letter = alphabet[(sum) - 33];
             }
-        }else {
-            if (Character.isUpperCase(c)){
-                letter = Character.toUpperCase(al[sum]);
-            }else {
-                letter = al[sum];
+        } else {
+            if (Character.isUpperCase(c)) {
+                letter = Character.toUpperCase(alphabet[sum]);
+            } else {
+                letter = alphabet[sum];
             }
         }
         return letter;
